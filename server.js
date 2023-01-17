509e992b9415fee9ba7dcfdd66a8aed7425d6bca
akshatgadodia <akshatgadodia@gmail.com>
2023-01-17 17:06:42 +0530

    Build Created

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 const jsonServer = require('json-server');
-// const cors = require('cors');
+const cors = require('cors');
+
 const server = jsonServer.create();
 const router = jsonServer.router('./data/db.json');
+
+const port = process.env.PORT || 8000;
+
 const middlewares = jsonServer.defaults({
     static: './build'
 });
-const port = process.env.PORT || 8000;
+
 server.use(middlewares);
 server.use(
     jsonServer.rewriter({
         '/api/*': '/$1',
     })
 );
-// server.use(cors());
+server.use(cors());
 server.use(router);
 server.listen(port, () => {
     console.log(`JSON Server is running on port ${port}`);
