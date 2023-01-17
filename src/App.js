1a1cd815732fc422b4c315a2d51a932b64116b54
akshatgadodia <akshatgadodia@gmail.com>
2023-01-17 20:04:03 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import './App.css';
 import Homepage from './Pages/Homepage';
-import { Route, Routes } from "react-router-dom"
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Homepage notFound={false}/>} />
-      <Route path="*" element={<Homepage notFound={true} />} />
-    </Routes>
+    <Homepage/>
   );
 }
 
